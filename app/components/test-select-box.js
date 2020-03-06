a232fe020f9e5c954f81c009daddcff95b21c7c5
Andrew Kirwin <andrew.kirwin@gmail.com>
2020-03-06 20:32:42 +0000

    fix selection

--- app/components/test-select-box.js
+++ app/components/test-select-box.js
@@ -1,3 +1,10 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
-export default class TestSelectBox extends Component {}
+export default class TestSelectBox extends Component {
+  @action
+  handleSelection(value, sb) {
+    this.args.onSelect(value);
+    sb.close();
+  }
+}
