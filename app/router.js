31f0b4ff6041cff454b5a4512d1ec49cc402c98b
Andrew Kirwin <andrew.kirwin@gmail.com>
2021-04-20 19:06:05 +0100

    upgrade app

--- app/router.js
+++ app/router.js
@@ -1,5 +1,5 @@
 import EmberRouter from '@ember/routing/router';
-import config from 'example/config/environment';
+import config from 'select-box-test/config/environment';
 
 export default class Router extends EmberRouter {
   location = config.locationType;
